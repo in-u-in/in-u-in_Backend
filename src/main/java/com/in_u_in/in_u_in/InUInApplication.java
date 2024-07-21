8abeac2b0fbac37e53ad663e60b3cdf3a5842e47
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2024-07-21 20:46:52 +0900

    Initial setting

---
+++ src/main/java/com/in_u_in/in_u_in/InUInApplication.java
@@ -0,0 +1,13 @@
+package com.in_u_in.in_u_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InUInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InUInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in_u_in/in_u_in/InUInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in_u_in.in_u_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InUInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
